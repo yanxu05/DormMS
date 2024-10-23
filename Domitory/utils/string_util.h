9ac01e122f37cc2cd34bba0eb3116c16c3f03cd2
yanxu <yanxu8843@gmail.com>
2024-10-23 10:14:47 +0800

    Update string_util.h

--- Domitory/utils/string_util.h
+++ Domitory/utils/string_util.h
@@ -7,7 +7,16 @@
 #include <iostream>
 #include <xlocbuf>
 #include <codecvt>
+
 using namespace std;
+
+#ifdef _UNICODE
+#define string_t wstring
+#else
+#define string_t string
+#endif
+
+#define CODE_CONVERT_USE_WIN32
 
 class string_util
 {
