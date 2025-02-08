5dbe33c9d294c84ce8444664456ca40c9844faa9
yanxu <yanxu8843@gmail.com>
2025-02-08 12:06:56 +0800

    Update string_util.h

--- Domitory/utils/string_util.h
+++ Domitory/utils/string_util.h
@@ -67,5 +67,6 @@ public:
 public:
 	static std::wstring gbk2unicode(std::string str_gbk);
 	static std::string unicode2gbk(std::wstring str_unicode);
+	static std::string gbk2utf8(std::string str_gbk);
 };
 
