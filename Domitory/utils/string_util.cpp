bc8da5ec65f33103c3a9c30a1d20791b43841660
yanxu <yanxu8843@gmail.com>
2025-05-06 21:41:40 +0800

    Update string_util.cpp

--- Domitory/utils/string_util.cpp
+++ Domitory/utils/string_util.cpp
@@ -330,3 +330,550 @@ std::string string_util::to_astring(string_t str)
 #endif
 	return str_ret;
 }
+
+#if defined(CODE_CONVERT_USE_CPP11)
+std::wstring string_util::gbk2unicode(std::string str_gbk)
+{
+	if (str_gbk.size() <= 0) return std::wstring(L"");
+
+#if 0
+	try
+	{
+		wstring_convert<codecvt<wchar_t, char, mbstate_t>> gbk_cvt(new codecvt<wchar_t, char, mbstate_t>("chs"));
+		return gbk_cvt.from_bytes(str_gbk);
+	}
+	catch (exception& e)
+	{
+		cout << "string_util::gbk2unicode:" << e.what() << endl;
+	}
+
+	return std::wstring(L"");
+#endif
+
+	std::locale sys_locale("");
+
+	const char* data_from = str_gbk.c_str();
+	const char* data_from_end = str_gbk.c_str() + str_gbk.size();
+	const char* data_from_next = 0;
+
+	wchar_t* data_to = new wchar_t[str_gbk.size() + 1];
+	wchar_t* data_to_end = data_to + str_gbk.size() + 1;
+	wchar_t* data_to_next = 0;
+
+	wmemset(data_to, 0, str_gbk.size() + 1);
+
+	mbstate_t in_state = 0;
+	typedef std::codecvt<wchar_t, char, mbstate_t> convert_facet;
+	auto result = std::use_facet<convert_facet>(sys_locale).in(
+		in_state, data_from, data_from_end, data_from_next,
+		data_to, data_to_end, data_to_next);
+	if (result == convert_facet::ok)
+	{
+		std::wstring dst = data_to;
+		delete[] data_to;
+		return dst;
+	}
+	else
+	{
+		cout << "string_util::gbk2unicode: convert error." << endl;
+		delete[] data_to;
+		return std::wstring(L"");
+	}
+}
+
+std::string string_util::unicode2gbk(std::wstring str_unicode)
+{
+	if (str_unicode.size() <= 0) return std::string("");
+
+#if 0
+	try
+	{
+		wstring_convert<codecvt<wchar_t, char, mbstate_t>> gbk_cvt(new codecvt<wchar_t, char, mbstate_t>("chs"));
+		return gbk_cvt.to_bytes(str_unicode);
+	}
+	catch (exception& e)
+	{
+		cout << "string_util::unicode2gbk:" << e.what() << endl;
+	}
+
+	return std::string("");
+#endif
+
+	std::locale sys_locale("");
+
+	const wchar_t* data_from = str_unicode.c_str();
+	const wchar_t* data_from_end = str_unicode.c_str() + str_unicode.size();
+	const wchar_t* data_from_next = 0;
+
+	int wchar_size = 4;
+	char* data_to = new char[(str_unicode.size() + 1) * wchar_size];
+	char* data_to_end = data_to + (str_unicode.size() + 1) * wchar_size;
+	char* data_to_next = 0;
+
+	memset(data_to, 0, (str_unicode.size() + 1) * wchar_size);
+
+	typedef std::codecvt<wchar_t, char, mbstate_t> convert_facet;
+	mbstate_t out_state = 0;
+	auto result = std::use_facet<convert_facet>(sys_locale).out(
+		out_state, data_from, data_from_end, data_from_next,
+		data_to, data_to_end, data_to_next);
+	if (result == convert_facet::ok)
+	{
+		std::string dst = data_to;
+		delete[] data_to;
+		return dst;
+	}
+	else
+	{
+		cout << "string_util::unicode2gbk: convert error." << endl;
+		delete[] data_to;
+		return std::string("");
+	}
+}
+
+std::string string_util::gbk2utf8(std::string str_gbk)
+{
+	if (str_gbk.size() <= 0) return std::string("");
+
+#if 0
+	try
+	{
+		wstring_convert<codecvt_utf8<wchar_t>> utf8_cvt; //utf8=>unicode
+		wstring_convert<codecvt<wchar_t, char, mbstate_t>> gbk_cvt(new codecvt<wchar_t, char, mbstate_t>("chs")); //unicode=>gbk
+		wstring t = gbk_cvt.from_bytes(str_gbk);
+		return utf8_cvt.to_bytes(t);
+	}
+	catch (exception& e)
+	{
+		cout << "string_util::gbk2utf8:" << e.what() << endl;
+	}
+
+	return std::string("");
+#endif
+
+	std::wstring str_w = gbk2unicode(str_gbk);
+	return unicode2utf8(str_w);
+}
+
+std::string string_util::utf82gbk(std::string str_utf8)
+{
+	if (str_utf8.size() <= 0) return std::string("");
+
+#if 0
+	try
+	{
+		wstring_convert<codecvt_utf8<wchar_t>> utf8_cvt; //utf8=>unicode
+		wstring_convert<codecvt<wchar_t, char, mbstate_t>> gbk_cvt(new codecvt<wchar_t, char, mbstate_t>("chs")); //unicode=>gbk
+		wstring t = utf8_cvt.from_bytes(str_utf8);
+		return gbk_cvt.to_bytes(t);
+	}
+	catch (exception& e)
+	{
+		cout << "string_util::utf82gbk:" << e.what() << endl;
+	}
+
+	return std::string("");
+#endif
+
+	std::wstring str_w = utf82unicode(str_utf8);
+	return unicode2gbk(str_w);
+}
+
+std::string string_util::unicode2utf8(std::wstring str_unicode)
+{
+	if (str_unicode.size() <= 0) return std::string("");
+
+	try
+	{
+		std::wstring_convert<std::codecvt_utf8<wchar_t>> utf8_cvt;
+		return utf8_cvt.to_bytes(str_unicode);
+	}
+	catch (exception& e)
+	{
+		cout << "string_util::unicode2utf8:" << e.what() << endl;
+	}
+
+	return std::string("");
+}
+
+std::wstring string_util::utf82unicode(std::string str_utf8)
+{
+	if (str_utf8.size() <= 0) return std::wstring(L"");
+
+	try
+	{
+		std::wstring_convert<std::codecvt_utf8<wchar_t>> utf8_cvt;
+		return utf8_cvt.from_bytes(str_utf8);
+	}
+	catch (exception& e)
+	{
+		cout << "string_util::utf82unicode:" << e.what() << endl;
+	}
+
+	return std::wstring(L"");
+}
+
+#elif defined(CODE_CONVERT_USE_WIN32)
+std::string string_util::gbk2utf8(std::string str_gbk)
+{
+	if (str_gbk.size() <= 0) return std::string("");
+
+	//gbk=>unicode
+	int nLen = MultiByteToWideChar(CP_ACP, 0, str_gbk.c_str(), str_gbk.size(), NULL, 0);
+	if (nLen <= 0) std::string("");
+
+	wchar_t* lpsz_w = nullptr;
+	try
+	{
+		lpsz_w = new wchar_t[nLen + 2];
+		memset(lpsz_w, 0, (nLen + 2)*sizeof(wchar_t));
+	}
+	catch (bad_alloc& e)
+	{
+		cout << "gbk2utf8 : " << e.what() << endl;
+		return std::string("");
+	}
+
+	int nRtn = MultiByteToWideChar(CP_ACP, 0, str_gbk.c_str(), str_gbk.size(), lpsz_w, nLen);
+	if (nRtn != nLen)
+	{
+		delete[] lpsz_w;
+		return std::string("");
+	}
+	
+	//unicode=>utf8
+	int utf8_len = WideCharToMultiByte(CP_UTF8, 0, lpsz_w, nLen, NULL, 0, NULL, NULL);
+	if (utf8_len <= 0) return std::string("");
+
+	char* lpsz_m = nullptr;
+	try
+	{
+		lpsz_m = new char[utf8_len + 2];
+		memset(lpsz_m, 0, (utf8_len + 2)*sizeof(char));
+	}
+	catch (bad_alloc& e)
+	{
+		cout << "gbk2utf8 : " << e.what() << endl;
+		return std::string("");
+	}
+
+	nRtn = WideCharToMultiByte(CP_UTF8, 0, lpsz_w, nLen, lpsz_m, utf8_len, NULL, NULL);
+	delete[] lpsz_w;
+
+	if (nRtn != utf8_len)
+	{
+		delete[] lpsz_m;
+		return std::string("");
+	}
+
+	std::string str_utf8;
+	str_utf8 = lpsz_m;
+	delete[] lpsz_m;
+
+	return str_utf8;
+}
+
+std::string string_util::utf82gbk(std::string str_utf8)
+{
+	if (str_utf8.size() <= 0) return std::string("");
+
+	//utf8=>unicode
+	int nLen = MultiByteToWideChar(CP_UTF8, 0, str_utf8.c_str(), str_utf8.size(), NULL, 0);
+	if (nLen <= 0) return std::string("");
+
+	wchar_t* lpsz_w = nullptr;
+	try
+	{
+		lpsz_w = new wchar_t[nLen + 2];
+		memset(lpsz_w, 0, (nLen + 2)*sizeof(wchar_t));
+	}
+	catch (bad_alloc &e)
+	{
+		cout << "utf82gbk : " << e.what() << endl;
+		return std::string("");
+	}
+
+	int nRtn = MultiByteToWideChar(CP_UTF8, 0, str_utf8.c_str(), str_utf8.size(), lpsz_w, nLen);
+	if (nRtn != nLen)
+	{
+		delete[] lpsz_w;
+		return std::string("");
+	}
+
+	//unicode=>gbk
+	int gbk_len = WideCharToMultiByte(CP_ACP, 0, lpsz_w, nLen, NULL, 0, NULL, NULL);
+	if (gbk_len <= 0) return std::string("");
+
+	char* lpsz_m = nullptr;
+	try
+	{
+		lpsz_m = new char[gbk_len + 2];
+		memset(lpsz_m, 0, (gbk_len + 2)*sizeof(char));
+	}
+	catch (bad_alloc& e)
+	{
+		cout << "utf82gbk : " << e.what() << endl;
+		return std::string("");
+	}
+
+	nRtn = WideCharToMultiByte(CP_ACP, 0, lpsz_w, nLen, lpsz_m, gbk_len, NULL, NULL);
+	delete[] lpsz_w;
+
+	if (nRtn != gbk_len)
+	{
+		delete[] lpsz_m;
+		return std::string("");
+	}
+
+	std::string str_gbk;
+	str_gbk = lpsz_m;
+	delete[] lpsz_m;
+	return str_gbk;
+}
+
+std::wstring string_util::gbk2unicode(std::string str_gbk)
+{
+	if (str_gbk.size() <= 0) return std::wstring(L"");
+
+	int u_len = MultiByteToWideChar(CP_ACP, 0, str_gbk.c_str(), str_gbk.size(), NULL, 0);
+	if (u_len <= 0) return std::wstring(L"");
+
+	wchar_t* lpsz_w = nullptr;
+	try
+	{
+		lpsz_w = new wchar_t[u_len + 2];
+		memset(lpsz_w, 0, (u_len + 2)*sizeof(wchar_t));
+	}
+	catch (bad_alloc& e)
+	{
+		cout << "gbk2unicode : " << e.what() << endl;
+		return std::wstring(L"");
+	}
+
+	int nRtn = MultiByteToWideChar(CP_ACP, 0, str_gbk.c_str(), str_gbk.size(), lpsz_w, u_len);
+	if (nRtn != u_len)
+	{
+		delete[] lpsz_w;
+		return std::wstring(L"");
+	}
+
+	std::wstring str_unicode;
+	str_unicode = lpsz_w;
+	delete[] lpsz_w;
+	return str_unicode;
+}
+
+std::string string_util::unicode2gbk(std::wstring str_unicode)
+{
+	if (str_unicode.size() <= 0) return std::string("");
+	int gbk_len = WideCharToMultiByte(CP_ACP, 0, str_unicode.c_str(), str_unicode.size(), NULL, 0, NULL, NULL);
+	if (gbk_len <= 0) return std::string("");
+
+	char* lpsz_m = nullptr;
+	try
+	{
+		lpsz_m = new char[gbk_len + 2];
+		memset(lpsz_m, 0, (gbk_len + 2)*sizeof(char));
+	}
+	catch (bad_alloc& e)
+	{
+		cout << "unicode2gbk : " << e.what() << endl;
+		return std::string("");
+	}
+
+	int nRtn = WideCharToMultiByte(CP_ACP, 0, str_unicode.c_str(), str_unicode.size(), lpsz_m, gbk_len, NULL, NULL);
+	if (nRtn != gbk_len)
+	{
+		delete[] lpsz_m;
+		return std::string("");
+	}
+
+	std::string str_gbk;
+	str_gbk = lpsz_m;
+	delete[] lpsz_m;
+	return str_gbk;
+}
+
+std::wstring string_util::utf82unicode(std::string str_utf8)
+{
+	if (str_utf8.size() <= 0) return std::wstring(L"");
+	int u_len = MultiByteToWideChar(CP_UTF8, 0, str_utf8.c_str(), str_utf8.size(), NULL, 0);
+	if (u_len <= 0) return std::wstring(L"");
+
+	wchar_t* lpsz_w = nullptr;
+	try
+	{
+		lpsz_w = new wchar_t[u_len + 2];
+		memset(lpsz_w, 0, (u_len + 2)*sizeof(wchar_t));
+	}
+	catch (bad_alloc& e)
+	{
+		cout << "utf82unicode : " << e.what() << endl;
+		return std::wstring(L"");
+	}
+
+	int nRtn = MultiByteToWideChar(CP_UTF8, 0, str_utf8.c_str(), str_utf8.size(), lpsz_w, u_len);
+	if (nRtn != u_len)
+	{
+		delete[] lpsz_w;
+		return std::wstring(L"");
+	}
+
+	std::wstring str_unicode;
+	str_unicode = lpsz_w;
+	delete[] lpsz_w;
+	return str_unicode;
+}
+
+std::string string_util::unicode2utf8(std::wstring str_unicode)
+{
+	if (str_unicode.size() <= 0) return std::string("");
+	int utf8_len = WideCharToMultiByte(CP_UTF8, 0, str_unicode.c_str(), str_unicode.size(), NULL, 0, NULL, NULL);
+	if (utf8_len <= 0) return std::string("");
+
+	char* lpsz_m = nullptr;
+	try
+	{
+		lpsz_m = new char[utf8_len + 2];
+		memset(lpsz_m, 0, (utf8_len + 2)*sizeof(char));
+	}
+	catch (bad_alloc& e)
+	{
+		cout << "unicode2utf8 : " << e.what() << endl;
+		return std::string("");
+	}
+
+	int nRtn = WideCharToMultiByte(CP_UTF8, 0, str_unicode.c_str(), str_unicode.size(), lpsz_m, utf8_len, NULL, NULL);
+	if (nRtn != utf8_len)
+	{
+		delete[] lpsz_m;
+		return std::string("");
+	}
+
+	std::string str_utf8;
+	str_utf8 = lpsz_m;
+	delete[] lpsz_m;
+	return str_utf8;
+}
+
+#elif defined(CODE_CONVERT_USE_ICONV)
+#include "iconv_inc.h"
+std::wstring string_util::gbk2unicode(std::string str_gbk)
+{
+	int nSize = str_gbk.size() + 2;
+	wchar_t *pBuf = new wchar_t[nSize];
+	if (!pBuf) return std::wstring(L"");
+	wmemset(pBuf, 0, nSize);
+
+	int ret = code_convert("UCS-2LE//TRANSLIT//IGNORE", "GBK", str_gbk.c_str(), str_gbk.size(), (char*)pBuf, nSize * sizeof(wchar_t));
+	if (ret <= 0)
+	{
+		delete[] pBuf;
+		return std::wstring(L"");
+	}
+
+	std::wstring str_unicode;
+	str_unicode = pBuf;
+	delete[] pBuf;
+	return str_unicode;
+}
+
+std::string string_util::unicode2gbk(std::wstring str_unicode)
+{
+	int nSize = str_unicode.size() * sizeof(wchar_t) + 2;
+	char *pBuf = new char[nSize];
+	if (!pBuf) return std::string("");
+	memset(pBuf, 0, nSize);
+
+	int ret = code_convert("GBK//TRANSLIT//IGNORE", "UCS-2LE", (char*)str_unicode.c_str(), str_unicode.size() * sizeof(wchar_t), pBuf, nSize);
+	if (ret <= 0)
+	{
+		delete[] pBuf;
+		return string("");
+	}
+
+	std::string str_gbk;
+	str_gbk = pBuf;
+	delete[] pBuf;
+	return str_gbk;
+}
+
+std::string string_util::gbk2utf8(std::string str_gbk)
+{
+	int nSize = str_gbk.size() * 2 + 2;
+	char *pBuf = new char[nSize];
+	if (!pBuf) return std::string("");
+	memset(pBuf, 0, nSize);
+
+	int ret = code_convert("UTF-8//TRANSLIT//IGNORE", "GBK", str_gbk.c_str(), str_gbk.size(), pBuf, nSize);
+	if (ret <= 0)
+	{
+		delete[] pBuf;
+		return std::string("");
+	}
+
+	std::string str_utf8;
+	str_utf8 = pBuf;
+	delete[] pBuf;
+	return str_utf8;
+}
+
+std::string string_util::utf82gbk(std::string str_utf8)
+{
+	int nSize = str_utf8.size() * 2 + 2;
+	char *pBuf = new char[nSize];
+	if (!pBuf) return std::string("");
+	memset(pBuf, 0, nSize);
+
+	int ret = code_convert("GBK//TRANSLIT//IGNORE", "UTF-8", str_utf8.c_str(), str_utf8.size(), pBuf, nSize);
+	if (ret <= 0)
+	{
+		delete[] pBuf;
+		return std::string("");
+	}
+
+	std::string str_gbk;
+	str_gbk = pBuf;
+	delete[] pBuf;
+	return str_gbk;
+}
+
+std::string string_util::unicode2utf8(std::wstring str_unicode)
+{
+	int nSize = str_unicode.size() * sizeof(wchar_t) + 2;
+	char *pBuf = new char[nSize];
+	if (!pBuf) return std::string("");
+	memset(pBuf, 0, nSize);
+
+	int ret = code_convert("UTF-8//TRANSLIT//IGNORE", "UCS-2LE", (char*)str_unicode.c_str(), str_unicode.size() * sizeof(wchar_t), pBuf, nSize);
+	if (ret <= 0)
+	{
+		delete[] pBuf;
+		return std::string("");
+	}
+
+	std::string str_utf8;
+	str_utf8 = pBuf;
+	delete[] pBuf;
+	return str_utf8;
+}
+
+std::wstring string_util::utf82unicode(std::string str_utf8)
+{
+	int nSize = str_utf8.size() + 2;
+	wchar_t *pBuf = new wchar_t[nSize];
+	if (!pBuf) return std::wstring(L"");
+	wmemset(pBuf, 0, nSize);
+
+	int ret = code_convert("UCS-2LE//TRANSLIT//IGNORE", "UTF-8", str_utf8.c_str(), str_utf8.size(), (char*)pBuf, nSize * sizeof(wchar_t));
+	if (ret <= 0)
+	{
+		delete[] pBuf;
+		return std::wstring(L"");
+	}
+
+	std::wstring str_unicode;
+	str_unicode = pBuf;
+	delete[] pBuf;
+	return str_unicode;
+}
